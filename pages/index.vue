c0892ab22105230dbd79fac5f7c1205b9482b652
hid_aoyagi <ha.language@gmail.com>
2021-09-06 17:50:37 +0900

    first commit.

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <Tutorial /> -->
+    <input type="text" />
+    <button @click="onClick">押したら乱数が出る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      alert(Math.random())
+    }
+  }
+}
+</script>
